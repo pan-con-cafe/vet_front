b78f98dfe262f513da8bb5fbef33f8e91ea0df72
Pancito <pansiendopan@gmail.com>
2026-07-27 21:23:41 -0500

    ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,16 @@ import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-    { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
+  { path: 'login', title: 'Iniciar sesión',loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
   { path: '', canActivate: [authGuard], children: [
       { path: '', redirectTo: 'clientes', pathMatch: 'full' },
-      { path: 'clientes', loadComponent: () => import('./pages/clientes/clientes.component').then(m => m.ClientesComponent) },
-      { path: 'mascotas', loadComponent: () => import('./pages/mascotas/mascotas.component').then(m => m.MascotasComponent) },
-      { path: 'citas', loadComponent: () => import('./pages/citas/citas.component').then(m => m.CitasComponent) },
-      { path: 'mascotas/:id', canActivate: [authGuard], loadComponent: () => import('./pages/mascotas/mascota-perfil/mascota-perfil.component').then(m => m.MascotaPerfilComponent) },
-      { path: 'registro', canActivate: [authGuard], loadComponent: () => import('./pages/registro/registro.component').then(m => m.RegistroComponent) },
-      { path: 'mascotas/:id/medico', canActivate: [authGuard], loadComponent: () => import('./pages/mascotas/ficha-medica/ficha-medica.component').then(m => m.FichaMedicaComponent) },
-      { path: 'mascotas/:id/grooming', canActivate: [authGuard], loadComponent: () => import('./pages/mascotas/ficha-grooming/ficha-grooming.component').then(m => m.FichaGroomingComponent) },
+      { path: 'clientes', title: 'Veterinaria Huancayo',loadComponent: () => import('./pages/clientes/clientes.component').then(m => m.ClientesComponent) },
+      { path: 'mascotas', title: 'Veterinaria Huancayo',loadComponent: () => import('./pages/mascotas/mascotas.component').then(m => m.MascotasComponent) },
+      { path: 'citas', title: 'Veterinaria Huancayo',loadComponent: () => import('./pages/citas/citas.component').then(m => m.CitasComponent) },
+      { path: 'mascotas/:id', title: 'Perfil de mascota', canActivate: [authGuard], loadComponent: () => import('./pages/mascotas/mascota-perfil/mascota-perfil.component').then(m => m.MascotaPerfilComponent) },
+      { path: 'registro', title: 'Registrar mascota', canActivate: [authGuard], loadComponent: () => import('./pages/registro/registro.component').then(m => m.RegistroComponent) },
+      { path: 'mascotas/:id/medico', title: 'Ficha medica', canActivate: [authGuard], loadComponent: () => import('./pages/mascotas/ficha-medica/ficha-medica.component').then(m => m.FichaMedicaComponent) },
+      { path: 'mascotas/:id/grooming', title: 'Grooming', canActivate: [authGuard], loadComponent: () => import('./pages/mascotas/ficha-grooming/ficha-grooming.component').then(m => m.FichaGroomingComponent) },
   ]},
   { path: '**', redirectTo: 'login' }
 ];
